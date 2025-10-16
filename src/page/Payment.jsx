2f5ecbd96c7f1a34e07da992142532b034f0e9b2
Arshad-d8 <71703633+Arshad-d8@users.noreply.github.com>
2025-10-16 10:20:35 +0530

    changed

--- src/page/Payment.jsx
+++ src/page/Payment.jsx
@@ -6,10 +6,12 @@ export default function Payment() {
     const {pid}=useParams();
     const product=card.find(p=>p.id===parseInt(pid))
   return (
-    <div>
-      pay
+    <div className='content-center'>
+      <div className='content-center'>
+        pay
       <h1>{product.id}</h1>
       <h1>{product.name}</h1>
+      </div>
     </div>
   )
 }
